6d346dadb88fc11bef74114d7ea72363e2e745b0
Floris Boom <florisboom1@gmail.com>
2022-01-03 06:13:23 +0100

    shutdown sync temp

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
-import (
-	"github.com/florisboom/go-notion-manga-tracker/crawler"
-	"github.com/robfig/cron"
-)
-
 func main() {
-	c := cron.New()
-	c.AddFunc("@hourly", func() {
-		crawler.Sync()
-	})
+	// c := cron.New()
+	// c.AddFunc("@hourly", func() {
+	// 	crawler.Sync()
+	// })
 
-	c.Start()
+	// c.Start()
 	select {}
 }
